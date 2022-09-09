0c629a9ebd4acceae711055c514b314ff67b841a
user02 <user02@C22701>
2022-09-09 13:23:46 +0400

    7

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace lessonapp
     {
         static void Main(string[] args)
         {
-            Tree apple = new Appletree(1, "sda");
+            Tree apple = new Appletree(19, "sda");
             Tree grusha = new GrushaTree(-12, "Груща");
             Console.WriteLine(apple.Age);
             apple.Grow();
@@ -15,6 +15,17 @@ namespace lessonapp
             grusha.Grow();
             Console.WriteLine(grusha.Age);
             Console.ReadKey();
+            Tree antonovka = new Antonovka(5, "Антоновка");
+            Console.WriteLine(antonovka.Age);
+            antonovka.Grow();
+            Console.WriteLine(antonovka.Age);
+
+            Antonovka antonovka2 = new Antonovka(5, "Антоновка");
+            Console.WriteLine(antonovka2.Age);
+            antonovka2.Grow();
+            Console.WriteLine(antonovka2.Age);
+
+
 
 
         }
